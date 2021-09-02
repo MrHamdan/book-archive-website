2481c8c76c1dab5210350d0fea4337d7eb52a0f2
MrHamdan <mr.hamdanahmed@gmail.com>
2021-09-02 17:02:27 +0600

    fixing issues

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const bookContainer = document.getElementById('book-container');
 const backgroundImage = document.getElementById('bg-img')
 backgroundImage.style.backgroundImage = "url('images/hlezTG.jpg')"
 
@@ -7,19 +8,16 @@ const searchBook = () => {
   const searchText = searchField.value;
 
   // Error Handle
-  if (searchText == 0) {
-    const bookContainer = document.createElement('book-container');
+  if (searchText === '') {
     const errorMessage = document.getElementById('result-found-container');
-    errorMessage.textContent = "";
     spinner.classList.add('d-none');
     errorMessage.innerHTML = `<h2 class="fw-bold">Please enter a book name!!!</h2>`;
-    bookContainer.classList.add('d-none');
+    bookContainer.textContent = '';
   }
   else {
     const errorMessage = document.getElementById('result-found-container');
-    errorMessage.innerText = "";
+    errorMessage.innerText = '';
     spinner.classList.remove('d-none');
-
     // Load data
     const url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
@@ -33,20 +31,18 @@ const searchBook = () => {
 
 // Display data
 const displaySearchResult = (data) => {
-  const bookContainer = document.createElement('book-container');
+  bookContainer.textContent = '';
   const errorMessage = document.getElementById('result-found-container');
-  errorMessage.textContent = "";
+  errorMessage.textContent = '';
   // error handle
   if (data.num_found === 0) {
-    errorMessage.innerText = "No Data Found";
-    errorMessage.textContent = "";
+    alert('No data Found');
+    errorMessage.textContent = '';
     spinner.classList.add('d-none');
-    bookContainer.classList.add('d-none');
   }
   else {
-    errorMessage.innerText = "";
+    errorMessage.innerText = '';
     // show how many search result
-    const bookContainer = document.getElementById('book-container');
     const showResult = document.getElementById('result-found-container');
     const totalBooks = data.num_found;
     const books = data.docs;
